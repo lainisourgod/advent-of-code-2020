9872e3d5d182b5813644ea0f1a45c103feab47d0
meandmymind <beaverloverock@gmail.com>
2021-04-20 21:28:13 +0700

    Day 6: separate input reading from 1st star solution

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,10 +1,17 @@
 use std::collections::HashSet;
 use std::fs::read_to_string;
 
-fn read_input(filename: &str) -> Vec<HashSet<char>> {
+fn read_input(filename: &str) -> Vec<String> {
     read_to_string(filename)
         .unwrap_or_else(|_| panic!("No file named {} found", filename))
         .split("\n\n")
+        .map(str::to_owned)
+        .collect()
+}
+
+fn find_what_everyone_answered(answers: Vec<String>) -> Vec<HashSet<char>> {
+    answers
+        .iter()
         .map(|group_answers| {
             group_answers
                 .split_whitespace()
@@ -17,10 +24,12 @@ fn read_input(filename: &str) -> Vec<HashSet<char>> {
 fn main() {
     let answers = read_input("input.txt");
 
-    let total_answers: usize = answers
+    let answers_anyone = find_what_everyone_answered(answers);
+
+    let total_answers_anyone: usize = answers_anyone
         .iter()
         .map(|group_answers| group_answers.len())
         .sum();
 
-    println!("Total answer count {}", &total_answers);
+    println!("Total answer count {}", &total_answers_anyone);
 }
