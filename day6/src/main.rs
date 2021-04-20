55520b98291b24e2b9445457d2a0712f65db5392
meandmymind <beaverloverock@gmail.com>
2021-04-20 21:42:32 +0700

    Day 6: 2nd star 🌟

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -9,7 +9,7 @@ fn read_input(filename: &str) -> Vec<String> {
         .collect()
 }
 
-fn find_what_everyone_answered(answers: Vec<String>) -> Vec<HashSet<char>> {
+fn find_what_anyone_answered(answers: &[String]) -> Vec<HashSet<char>> {
     answers
         .iter()
         .map(|group_answers| {
@@ -21,15 +21,42 @@ fn find_what_everyone_answered(answers: Vec<String>) -> Vec<HashSet<char>> {
         .collect()
 }
 
+fn find_what_everyone_answered(answers: &[String]) -> Vec<HashSet<char>> {
+    answers
+        .iter()
+        .map(|group_answers| {
+            let mut group_answers: Vec<HashSet<char>> = group_answers
+                .split_whitespace()
+                .map(|person_answers| person_answers.chars().collect::<HashSet<char>>())
+                .collect();
+
+            let mut result = group_answers.pop().unwrap();
+            result.retain(|item| {
+                group_answers
+                    .iter()
+                    .all(|person_answers| person_answers.contains(item))
+            });
+            result
+        })
+        .collect()
+}
+
 fn main() {
     let answers = read_input("input.txt");
 
-    let answers_anyone = find_what_everyone_answered(answers);
+    let answers_anyone = find_what_anyone_answered(&answers);
 
     let total_answers_anyone: usize = answers_anyone
         .iter()
         .map(|group_answers| group_answers.len())
         .sum();
+    println!("Total anyone answer count {}", &total_answers_anyone);
 
-    println!("Total answer count {}", &total_answers_anyone);
+    let answers_everyone = find_what_everyone_answered(&answers);
+
+    let total_answers_everyone: usize = answers_everyone
+        .iter()
+        .map(|group_answers| group_answers.len())
+        .sum();
+    println!("Total everyone answer count {}", &total_answers_everyone);
 }
